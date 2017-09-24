06f537056810c14b4b7839ba971eecde59960129
Arpit Agarwal <arpit.agarwal@zomato.com>
2017-09-24 09:06:49 +0530

    added categories for autolayout

---
+++ List/Commons/Categories/UIButton+HitTest.h
@@ -0,0 +1,15 @@
+//
+//  UIButton+UIButton_HitTest.h
+//  List
+//
+//  Created by Arpit Agarwal on 14/09/17.
+//  Copyright © 2017 acyooman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (HitTest)
+
+@property(nonatomic, assign) UIEdgeInsets hitTestEdgeInsets;
+
+@end
